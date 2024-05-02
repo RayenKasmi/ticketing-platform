448a81b21303b2b0ce29e322791e7eaac3198120
Iyed <iyedabdelli73@gmail.com>
2024-05-02 23:43:34 +0100

    Create layout template

---
+++ templates/sections/scripts.php
@@ -0,0 +1,7 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Ostrobogulous/ticketing-platform/lib/easing/easing.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Ostrobogulous/ticketing-platform/lib/waypoints/waypoints.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Ostrobogulous/ticketing-platform/lib/lightbox/js/lightbox.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Ostrobogulous/ticketing-platform/lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/Ostrobogulous/ticketing-platform/Static/JS/main.js"></script>
