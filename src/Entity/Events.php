b7ce8ed8035cf74a48eb0c85e582bea2e14eb492
RayenKasmi <rayenekasmi0@gmail.com>
2024-05-02 22:30:13 +0100

    Implemented database setup with Doctrine and user authentication system
    - Initialized database schema using Doctrine ORM
    - Created User entity with properties for user registration
    - Configured registration, login, and logout functionality
    - Implemented user authentication

---
+++ src/Entity/Events.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventsRepository::class)]
+class Events
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $venue = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $shortDescription = null;
+
+    #[ORM\Column(length: 10000)]
+    private ?string $longDescription = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $organizer = null;
+
+    #[ORM\Column]
+    private ?int $totalTickets = null;
+
+    #[ORM\Column]
+    private ?int $availableTickets = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $startSellTime = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $eventDate = null;
+
+    #[ORM\Column]
+    private ?int $ticketPrice = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imagePath = null;
+
+    #[ORM\ManyToOne(inversedBy: 'events')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Categories $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getVenue(): ?string
+    {
+        return $this->venue;
+    }
+
+    public function setVenue(string $venue): static
+    {
+        $this->venue = $venue;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->shortDescription;
+    }
+
+    public function setShortDescription(string $shortDescription): static
+    {
+        $this->shortDescription = $shortDescription;
+
+        return $this;
+    }
+
+    public function getLongDescription(): ?string
+    {
+        return $this->longDescription;
+    }
+
+    public function setLongDescription(string $longDescription): static
+    {
+        $this->longDescription = $longDescription;
+
+        return $this;
+    }
+
+    public function getOrganizer(): ?string
+    {
+        return $this->organizer;
+    }
+
+    public function setOrganizer(string $organizer): static
+    {
+        $this->organizer = $organizer;
+
+        return $this;
+    }
+
+    public function getTotalTickets(): ?int
+    {
+        return $this->totalTickets;
+    }
+
+    public function setTotalTickets(int $totalTickets): static
+    {
+        $this->totalTickets = $totalTickets;
+
+        return $this;
+    }
+
+    public function getAvailableTickets(): ?int
+    {
+        return $this->availableTickets;
+    }
+
+    public function setAvailableTickets(int $availableTickets): static
+    {
+        $this->availableTickets = $availableTickets;
+
+        return $this;
+    }
+
+    public function getStartSellTime(): ?\DateTimeInterface
+    {
+        return $this->startSellTime;
+    }
+
+    public function setStartSellTime(\DateTimeInterface $startSellTime): static
+    {
+        $this->startSellTime = $startSellTime;
+
+        return $this;
+    }
+
+    public function getEventDate(): ?\DateTimeInterface
+    {
+        return $this->eventDate;
+    }
+
+    public function setEventDate(\DateTimeInterface $eventDate): static
+    {
+        $this->eventDate = $eventDate;
+
+        return $this;
+    }
+
+    public function getTicketPrice(): ?int
+    {
+        return $this->ticketPrice;
+    }
+
+    public function setTicketPrice(int $ticketPrice): static
+    {
+        $this->ticketPrice = $ticketPrice;
+
+        return $this;
+    }
+
+    public function getImagePath(): ?string
+    {
+        return $this->imagePath;
+    }
+
+    public function setImagePath(string $imagePath): static
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Categories
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Categories $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Notifications.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NotificationsRepository::class)]
+class Notifications
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sender = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'notifications')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\Column(type: Types::BOOLEAN)]
+    private bool $is_read = false; // Default value set to false
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSender(): ?string
+    {
+        return $this->sender;
+    }
+
+    public function setSender(string $sender): static
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function isRead(): bool
+    {
+        return $this->is_read;
+    }
+
+    public function setRead(bool $is_read): static
+    {
+        $this->is_read = $is_read;
+
+        return $this;
+    }
+}
