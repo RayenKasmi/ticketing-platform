0f31f9f1ac2680cc6f799a48373ca4f95aed7873
RayenKasmi <rayenekasmi0@gmail.com>
2024-05-03 16:36:40 +0100

    Enhanced user authentication mechanism to prevent login attempts for deleted accounts.
    - Upon account deletion, implemented logic to prevent the user from logging in even if their account remains in the database.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -41,6 +42,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isActive = true;
 
     /**
      * @var Collection<int, Notifications>
@@ -157,6 +161,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function setActive(bool $isActive): static
+    {
+        $this->isActive = $isActive;
 
         return $this;
     }
