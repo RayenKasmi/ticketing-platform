8bd9bc04d3e14091855dab2cde52170c0c2bb60d
RayenKasmi <rayenekasmi0@gmail.com>
2024-05-10 03:01:54 +0100

    Moved the search query creation to the eventRepo, used doctrine query builder to create the search query instead of native sql query

--- src/Controller/SearchEventsController.php
+++ src/Controller/SearchEventsController.php
@@ -3,22 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\CategoriesRepository;
+use App\Repository\EventsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\Query\ResultSetMappingBuilder;
 
 class SearchEventsController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
     private CategoriesRepository $categoryRepository;
+    private EventsRepository $eventRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, CategoriesRepository $categoryRepository)
+    public function __construct(CategoriesRepository $categoryRepository, EventsRepository $eventRepository)
     {
-        $this->entityManager = $entityManager;
         $this->categoryRepository = $categoryRepository;
+        $this->eventRepository = $eventRepository;
     }
 
     #[Route('/search', name: 'app_search_events', methods: ['GET'])]
@@ -31,15 +31,7 @@ class SearchEventsController extends AbstractController
         }
 
         try {
-            $rsm = new ResultSetMappingBuilder($this->entityManager);
-            $rsm->addRootEntityFromClassMetadata('App\Entity\Events', 'e');
-            $searchedEvents = $this->entityManager->createNativeQuery("
-            SELECT *
-            FROM events e
-            WHERE MATCH(e.short_description, e.long_description, e.name, e.venue, e.organizer) AGAINST (:searchTerm IN NATURAL LANGUAGE MODE)
-        ", $rsm)
-                ->setParameter('searchTerm', $term)
-                ->getResult();
+            $searchedEvents = $this->eventRepository->searchEvents($term);
             $categories = $this->categoryRepository->findAll();
 
             return $this->render('search_events/index.html.twig', [
