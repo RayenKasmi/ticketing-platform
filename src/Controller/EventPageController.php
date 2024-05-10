0964507cbfe58e7f0010ca49dd23b6f3bc8b787b
Iyed <iyedabdelli73@gmail.com>
2024-05-10 03:39:38 +0100

    Update event prices to match the currency set on the website

--- src/Controller/EventPageController.php
+++ src/Controller/EventPageController.php
@@ -4,26 +4,33 @@ namespace App\Controller;
 
 use App\Entity\Events;
 use App\Form\EventReservationType;
+use App\Service\CurrencyConverterService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EventPageController extends AbstractController
 {
 
     private EntityManagerInterface $entityManager;
+    private CurrencyConverterService $currencyConverter;
     private int $MAX_QUERY_RESULT = 5;
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, CurrencyConverterService $currencyConverterService)
     {
         $this->entityManager = $entityManager;
+        $this->currencyConverter = $currencyConverterService;
     }
     #[Route('/event/{id}', name: 'app_event_page')]
-    public function index(Events $event=null): Response
+    public function index(Events $event=null, SessionInterface $session): Response
     {
         if (!$event) {
             return new Response('The event does not exist', Response::HTTP_NOT_FOUND);
         }
+
+
+
         // Fetch up to N events from the same category of the current event
         $currentCategoryEvents = $this->entityManager->getRepository(Events::class)->createQueryBuilder('e')
             ->where('e.category = :category and e.id != :id')
@@ -32,6 +39,15 @@ class EventPageController extends AbstractController
             ->setMaxResults($this->MAX_QUERY_RESULT)
             ->getQuery()
             ->getResult();
+
+        $currency = $session->get('currency', 'USD');
+
+        if ($currency !== 'USD') {
+            $event->setTicketPrice($this->currencyConverter->convertPrice($event->getTicketPrice(), $currency));
+            foreach ($currentCategoryEvents as $event) {
+                $event->setTicketPrice($this->currencyConverter->convertPrice($event->getTicketPrice(), $currency));
+            }
+        }
 
         $form = $this->createForm(EventReservationType::class);
 
